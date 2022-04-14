91163f87af41d91235e12b26f814e5bd347c3801
Ay-slim <ayooluwaadedipe@gmail.com>
2022-04-15 00:22:21 +0100

    Recursive isEven

--- chapter_3_functions.js
+++ chapter_3_functions.js
@@ -14,3 +14,12 @@ function minimum(a, b) {
     }
 }
 
+function isEven(n) {
+    if(n < 0 || typeof n !== 'number') throw new Error('Only pass in a positive whole number');
+    if(n===0) return true;
+    if(n===1) return false;
+    return isEven(n-2);
+}
+
+//console.log(minimum(6,6))
+console.log(isEven(2))
