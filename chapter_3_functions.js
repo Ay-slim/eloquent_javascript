0b52df621ac0b7f92eac361e84b1f17357b44611
Ay-slim <ayooluwaadedipe@gmail.com>
2022-04-15 00:33:55 +0100

    Count chars function

--- chapter_3_functions.js
+++ chapter_3_functions.js
@@ -15,11 +15,21 @@ function minimum(a, b) {
 }
 
 function isEven(n) {
-    if(n < 0 || typeof n !== 'number') throw new Error('Only pass in a positive whole number');
-    if(n===0) return true;
-    if(n===1) return false;
+    if (n < 0 || typeof n !== 'number') throw new Error('Only pass in a positive whole number');
+    if (n===0) return true;
+    if (n===1) return false;
     return isEven(n-2);
 }
 
-//console.log(minimum(6,6))
-console.log(isEven(2))
+function countChars(mainString, charToCheck) {
+    if (typeof mainString !== 'string' || typeof charToCheck !== 'string') throw new Error('Function only takes a string');
+    let numberOfOccurrences = 0;
+    for (let j=0; j < mainString.length; j++) {
+        if (mainString[j] === charToCheck) numberOfOccurrences++;
+    }
+    return numberOfOccurrences
+}
+
+//console.log(minimum(6,6));
+//console.log(isEven(2));
+console.log(countChars('adabacebecebe', 4));
