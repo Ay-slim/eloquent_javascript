c2045911e24adc4c88d924e67e69bda9607774c3
Ay-slim <ayooluwaadedipe@gmail.com>
2022-04-14 18:35:00 +0100

    Chess board

--- chapter_1.js
+++ chapter_1.js
@@ -1,8 +1,8 @@
 function basicPrint() {
-    let toPrint = ''
+    let toPrint = '';
     for (let j = 0; j<7; j++) {
-        toPrint+='#'
-        console.log(toPrint)
+        toPrint+='#';
+        console.log(toPrint);
     }
 }
 
@@ -11,16 +11,41 @@ function fizzBuzz() {
         let divisibleBy3 = j%3 === 0;
         let divisibleBy5 = j%5 === 0;
         if(divisibleBy3 && divisibleBy5) {
-            console.log('fizzbuz')
+            console.log('fizzbuz');
         } else if(divisibleBy5){
-            console.log('buzz')
+            console.log('buzz');
         } else if(divisibleBy3){
-            console.log('fizz')
+            console.log('fizz');
         }  else {
-            console.log(j)
+            console.log(j);
         }
     }
 }
 
-//basicPrint()
-fizzBuzz()
+function chessBoard(n){
+    let outerSwitch;
+    let toPrint='';
+    for(let j=1 ; j<n+1 ; j++){
+      outerSwitch = j%2 === 0;
+      for(let k=1 ; k<n+1 ; k++){
+        if(!outerSwitch){
+          if(k%2 !== 0) {
+            toPrint+='#';
+          }else{
+            toPrint+=' ';
+          }
+        } else {
+          if(k%2 === 0) {
+            toPrint+='#';
+          }else{
+            toPrint+=' ';
+          }
+        }
+      }
+      toPrint+='\n';
+    }
+    console.log(toPrint);
+  }
+//basicPrint();
+//fizzBuzz();
+chessBoard(8);
