fbdf8af34405015dd9663e15c4638c5b8b66350d
Ay-slim <ayooluwaadedipe@gmail.com>
2022-04-14 18:28:04 +0100

    Yeah, fizzbuzz, I know

--- chapter_1.js
+++ chapter_1.js
@@ -6,4 +6,21 @@ function basicPrint() {
     }
 }
 
-basicPrint()
+function fizzBuzz() {
+    for(let j=1; j<=100; j++) {
+        let divisibleBy3 = j%3 === 0;
+        let divisibleBy5 = j%5 === 0;
+        if(divisibleBy3 && divisibleBy5) {
+            console.log('fizzbuz')
+        } else if(divisibleBy5){
+            console.log('buzz')
+        } else if(divisibleBy3){
+            console.log('fizz')
+        }  else {
+            console.log(j)
+        }
+    }
+}
+
+//basicPrint()
+fizzBuzz()
