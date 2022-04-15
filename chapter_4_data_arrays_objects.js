019054c379fce5e9740684169a29d426cf5c491b
Ay-slim <ayooluwaadedipe@gmail.com>
2022-04-15 15:13:34 +0100

    A tad recursive

--- chapter_4_data_arrays_objects.js
+++ chapter_4_data_arrays_objects.js
@@ -4,7 +4,7 @@ const { checkArrayValidity,
 
 function range(start, end, step=1) {
     [start, end].map(checkNumberValidity);
-    
+
     let fullRange = [];
     for (let j = start; j <= end; j+=step) {
         fullRange.push(j);
@@ -28,5 +28,41 @@ function reverseArray(arrayToReverse) {
     return reversedArray;
 }
 
-reverseArray('bruhh');
+function reverseArrayInPlace (arrayToReverseInPlace) {
+    checkArrayValidity(arrayToReverseInPlace);
+    const arrayLength = arrayToReverseInPlace.length;
+    let startIndex = 0, endIndex = arrayLength - 1;
+    const stopIndex = Math.floor((arrayLength/2)-1);
+    return recursiveReversal(startIndex, endIndex, stopIndex, arrayToReverseInPlace);
+}
+
+function recursiveReversal (startIndex, endIndex, stopIndex, arrayToReverse) {
+    if (startIndex>stopIndex) return arrayToReverse;
+    [arrayToReverse[startIndex], arrayToReverse[endIndex]] = [arrayToReverse[endIndex], arrayToReverse[startIndex]];
+    return recursiveReversal(startIndex+1, endIndex-1, stopIndex, arrayToReverse);
+}
+
+function spawnObjRecursively (presentIndex, lastIndex, arrayToConvert) {
+    if (presentIndex === lastIndex) {
+        return {
+            value: arrayToConvert[lastIndex],
+            rest: null
+        }
+    }
+    return {
+        value: arrayToConvert[presentIndex],
+        rest: spawnObjRecursively(presentIndex+1, lastIndex, arrayToConvert)
+    }
+}
+
+function arrayToList (arrayToConvert) {
+    const lastIndex = arrayToConvert.length-1;
+    return spawnObjRecursively(0, lastIndex, arrayToConvert)
+}
+
+const testArray = ['brih', 'brah', 'breh', 'bruh', 'broh', 'bryh'];
+
 //console.log(addAll(range(1,10, 1)));
+// console.log(reverseArray(testArray));
+// console.log(reverseArrayInPlace(testArray));
+console.log(arrayToList(['3', '4', '8']))
