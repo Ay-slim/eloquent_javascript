09db760fd80c84ab2d9a3a63851cdcc8a2f57f4c
Ay-slim <ayooluwaadedipe@gmail.com>
2022-04-17 15:42:31 +0100

    Loop function. To-do: Get more context

--- chapter_5_higher_order_functions.js
+++ chapter_5_higher_order_functions.js
@@ -2,5 +2,14 @@ function flatten (nestedArray) {
     return nestedArray.reduce((finalUnnestedArray, element) => finalUnnestedArray.concat(element))
 }
 
+function loop (argList) {
+    const {start, end, step, value, testFunc, bodyFunc, updateFunc} = argList;
+    for (let j = start; j<=end; j = j+step) {
+        if(!testFunc(value)) break;
+        bodyFunc(value);
+        return updateFunc(value);
+    }
+}
+
 // const testNestedArray = [[1536, 1541], [1542, 1548], [1549, 1563], [1564, 1565]];
 // console.log(flatten(testNestedArray))
