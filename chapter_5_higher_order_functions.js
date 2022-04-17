1ce578b882730a950b56b75f1970ea2c8ac64f78
Ay-slim <ayooluwaadedipe@gmail.com>
2022-04-17 15:42:53 +0100

    Every function

--- chapter_5_higher_order_functions.js
+++ chapter_5_higher_order_functions.js
@@ -11,5 +11,15 @@ function loop (argList) {
     }
 }
 
+function every (arrayToTest, func) {
+    for(el of arrayToTest) {
+        if(!func(el)) return false
+    }
+    return true
+}
+
+const testTestFunction = p => p > 5;
+console.log(every([6,7,8,9], testTestFunction)) 
+
 // const testNestedArray = [[1536, 1541], [1542, 1548], [1549, 1563], [1564, 1565]];
 // console.log(flatten(testNestedArray))
